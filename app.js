3e8b396ced4a67aa3f442391eea3e783cb67dabb
Abraham Rios <arhrone@gmail.com>
2025-09-11 00:40:59 -0700

    Agrega una función que crea una lista de los nombres

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ let amigos = [];
 
 
 /* Esta función nos permite tomar el valor que ingrese el usuario, verificar que sea un valor válido
-y agregarlo a la lista */
+y agregarlo al array */
 function agregarAmigo() {
     //aquí guardamos en la variable el valor que ingresó el usuario
     let amigoAgregado = document.getElementById('amigo').value; 
     console.log(amigoAgregado); 
+    //confirma que el input no esté vacío
     if (amigoAgregado === "") {
         alert ("Por favor, inserte un nombre"); 
     }
@@ -17,7 +18,18 @@ function agregarAmigo() {
             amigos.push(amigoAgregado);
             //limpia la caja de texto al presionar el botón añadir
             document.getElementById('amigo').value = ""; 
+            //llama a la función que agrega el nombre a una lista visible en la interfaz
+            agregarAmigosLista();
         }
 }
 
 console.log(amigos);
+
+//función que toma los elementos del array y los agrega a una lista
+function agregarAmigosLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; 
+    for (let i = 0; i < amigos.length; i++) {
+        lista.innerHTML += `<li>${amigos[i]}</li>`;
+    }
+}
